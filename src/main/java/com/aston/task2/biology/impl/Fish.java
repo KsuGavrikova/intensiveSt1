8dca71682e9c2e189f21cf530a18295e7e0f728f
Kseniya Haurykava <ksugavrikova@gmail.com>
2024-05-23 18:57:23 +0300

    updated task2 part1

--- src/main/java/com/aston/task2/biology/impl/Fish.java
+++ src/main/java/com/aston/task2/biology/impl/Fish.java
@@ -4,4 +4,8 @@ import com.aston.task2.biology.Animal;
 import com.aston.task2.biology.specialty.Watercraft;
 
 public class Fish implements Animal, Watercraft {
+    @Override
+    public String toString() {
+        return "Рыба";
+    }
 }
